19b51bb16868f4ac406b184b8cef14ba2e9c236c
Alex Foster <alexfost94@gmail.com>
2021-11-04 15:47:07 -0600

    Cleanup to Oauth flow and grabbing the url code

--- client/src/components/Dashboard-component.vue
+++ client/src/components/Dashboard-component.vue
@@ -4,6 +4,9 @@
           <div class="col-sm-12">
               <h1>Song Search</h1>
               <input type="text">
+              {{state.accessToken}}
+              {{state.refreshToken}}
+              <p>{{state.expiresIn}}</p>
           </div>
       </div>
   </div>
@@ -15,62 +18,76 @@ import { AppState } from '../AppState'
 import { computed, onMounted, reactive } from 'vue'
 import { api } from '../services/AxiosService'
 export default {
+    props : {
+        code: {
+            type: String,
+            required: true
+        }
+    },
   setup() {
       const state = reactive ({
-          code: '',
+          code: new URLSearchParams(window.location.search).get('code'),
           accessToken: '',
           refreshToken: '',
           expiresIn: ''
       })
-    //   onMounted (async () => {
-    //       if(state.code != undefined) {
-    //           console.log('onmounted', state.code)
-    //           const data = await api.post('/login', {code : state.code})
-    //           console.log(data)
-    //       }
-    //   })
+      console.log('dashboard component code', state.code)
+      console.log('hello')
+      onMounted (async () => {
+          if(state.code != undefined) {
+              console.log('onmounted', state.code)
+              const res = await api.post('/login', {code : state.code})
+              state.accessToken = res.data.accessToken
+              state.refreshToken = res.data.refreshToken
+              state.expiresIn = res.data.expiresIn
+              window.history.pushState({}, null, '/')
+          }
+      })
     return {
         state,
-        GET : async function(){
-    var get = {},
-    // the add function turns a key into an
-    // array if it is defined multiple times
-    add = function (key,value){
-        var cur = get[key]
-        if (cur.isArray){
-            get[key].push(value);
-        }else {
-            get[key] = [];
-            get[key].push(cur);
-            get[key].push(value);
-        }
-    },
-    search = document.location.search,
-    // decode special url characters like + into a (space)
-    decode = function (s) {
-      console.log(s)
-        return decodeURIComponent(s.split("+").join(" "));
-    };
+//         GET : async function(){
+//     var get = {},
+//     // the add function turns a key into an
+//     // array if it is defined multiple times
+//     add = function (key,value){
+//         var cur = get[key]
+//         if (cur.isArray){
+//             get[key].push(value);
+//         }else {
+//             get[key] = [];
+//             get[key].push(cur);
+//             get[key].push(value);
+//         }
+//     },
+//     search = document.location.search,
+//     // decode special url characters like + into a (space)
+//     decode = function (s) {
+//       console.log(s)
+//         return decodeURIComponent(s.split("+").join(" "));
+//     };
 
-    // The dreaded Regex
-    search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function (a,b,c) {
-        var key = decode(b)
-        if (get[key]){
-            add(decode(b),decode(c));
-        }else {
-            get[decode(b)] = decode(c);
-        }
-    });
-    console.log(get)
-    state.code = get.code
-    console.log('states code', state.code)
-    const res = await api.post('/login', {code: state.code})
-    console.log('loggin data', res.data)
-    state.accessToken = res.data.accessToken
-    state.refreshToken = res.data.refreshToken
-    state.expiresIn = res.data.expiresIn
-    return get;
-}()
+//     // The dreaded Regex
+//     search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function (a,b,c) {
+//         var key = decode(b)
+//         if (get[key]){
+//             add(decode(b),decode(c));
+//         }else {
+//             get[decode(b)] = decode(c);
+//         }
+//     });
+//     console.log(get)
+//     state.code = get.code
+//     console.log('states code', state.code)
+//     if(state.code) {
+//         const res = await api.post('/login', {code: state.code})
+//         console.log('loggin data', res.data)
+//         state.accessToken = res.data.accessToken
+//         state.refreshToken = res.data.refreshToken
+//         state.expiresIn = res.data.expiresIn
+//         window.history.pushState({}, null, '/')
+//     }
+//     return get;
+// }()
     //   user: computed(() => AppState.user),
     //   async login() {
     //     AuthService.loginWithPopup()
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div v-if="state.GET.code == undefined" class="home-card p-5 bg-white rounded elevation-3">
+    <div v-if="state.code == undefined" class="home-card p-5 bg-white rounded elevation-3">
       <a class="btn btn-success" :href="state.AUTH_URL">Spotify login</a>
     </div>
-    <router-view v-if="state.GET.code">
-      <Dashboard-component />
+    <router-view v-if="state.code">
+      <Dashboard-component :code='state.code' />
     </router-view>
   </div>
 </template>
@@ -24,45 +24,11 @@ export default {
     const router = useRouter()
     const state = reactive ({
       AUTH_URL: `https://accounts.spotify.com/authorize?client_id=83cd894c8d2646bab7384165d5be00ba&response_type=code&redirect_uri=http://localhost:8080/Dashboard&scopes=streaming%20user-read-email%20user-read-private%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`,
-      GET : function(){
-    var get = {},
-    // the add function turns a key into an
-    // array if it is defined multiple times
-    add = function (key,value){
-        var cur = get[key]
-        if (cur.isArray){
-            get[key].push(value);
-        }else {
-            get[key] = [];
-            get[key].push(cur);
-            get[key].push(value);
-        }
-    },
-    search = document.location.search,
-    // decode special url characters like + into a (space)
-    decode = function (s) {
-      console.log(s)
-        return decodeURIComponent(s.split("+").join(" "));
-    };
-
-    // The dreaded Regex
-    search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function (a,b,c) {
-        var key = decode(b)
-        if (get[key]){
-            add(decode(b),decode(c));
-        }else {
-            get[decode(b)] = decode(c);
-        }
-    });
-    console.log(get)
-    return get;
-    
-}(),
-    accessToken: '',
-    refreshToken: '',
-    expiresIn: ''
+      code: new URLSearchParams(window.location.search).get('code'),
+      accessToken: '',
+      refreshToken: '',
+      expiresIn: ''
     })
-    console.log(state.GET.code)
     // onMounted(async() => {
     //     const code = GET.code
     //     if (code == undefined) return
@@ -70,7 +36,6 @@ export default {
     //   })
     return {
       state,
-      
     }
   }
 }
